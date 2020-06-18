9128c8d5e2b2269621274264fe6ce0f4f0d1ecd7
Amelia <amelia@ameliaslaptop.lan>
2020-06-17 23:40:46 -0500

    added comments

--- kalman-plotting.R
+++ kalman-plotting.R
@@ -1,9 +1,7 @@
-library(infer)
-library(broom)
 library(dplyr)
 library(ggplot2)
 
-df <- read.csv("/home/amelia/go/src/kalman-filter/result.csv", na.strings = c("", "NA"))
+df <- read.csv("~/go/src/kalman-filter/result.csv", na.strings = c("", "NA"))
 attach(df)
 
 xVals = c(dataX, measurementX, predictionX)
@@ -15,5 +13,6 @@ newDf = data.frame(xVals, yVals, Labels)
 newDf %>%
   ggplot(aes(x = xVals, y = yVals)) + 
   geom_line(aes(color = Labels, linetype = Labels)) +
+  geom_point(aes(color = Labels)) +
   theme_classic(base_size=22, base_family="mono") +
   labs(x = "X Position", y = "Y Position", title = "Kalman Filter Position Tracking Example")
